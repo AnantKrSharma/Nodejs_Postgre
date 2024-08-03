5712fe3afac86768e00c346187a8969f3c2a4f24
Anant Kr Sharma <anantkrsharma01@gmail.com>
2024-08-03 17:15:07 +0530

    pg_queries

--- dist/index.js
+++ dist/index.js
@@ -18,7 +18,7 @@ dotenv_1.default.config();
 const client = new pg_1.Client({
     connectionString: process.env.POSTGRE
 });
-const createTable = () => __awaiter(void 0, void 0, void 0, function* () {
+const createUsersTable = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         yield client.connect();
         const result = yield client.query(`
@@ -39,13 +39,11 @@ const createTable = () => __awaiter(void 0, void 0, void 0, function* () {
         yield client.end();
     }
 });
-createTable();
-const insertInTable = (username, email, password) => __awaiter(void 0, void 0, void 0, function* () {
+const insertInUserTable = (username, email, password) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const result = yield client.query(`
-            INSERT INTO users (username, email, password) 
-            VALUES (hi, hi@gmail.com, 123);
-        `);
+        yield client.connect();
+        const insertQuery = 'INSERT INTO users (username, email, password) VALUES ($1, $2, $3);';
+        const result = yield client.query(insertQuery, [username, email, password]);
         console.log(result);
     }
     catch (error) {
@@ -55,4 +53,79 @@ const insertInTable = (username, email, password) => __awaiter(void 0, void 0, v
         yield client.end();
     }
 });
-insertInTable('aks', 'aks@gmail.com', '1_69420');
+const getUser = (email) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield client.connect();
+        const getUserQuery = 'SELECT * FROM users WHERE email = $1';
+        const result = yield client.query(getUserQuery, [email]);
+        if (result.rows.length > 0) {
+            console.log("User found:", result.rows[0]);
+            return;
+        }
+        else {
+            console.log(`No user found with the given email.`);
+            return null;
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+    finally {
+        yield client.end();
+    }
+});
+const createAddressTable = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield client.connect();
+        const query = `CREATE TABLE address (
+            id SERIAL PRIMARY KEY,
+            user_id INTEGER NOT NULL,
+            city VARCHAR(100) NOT NULL,
+            country VARCHAR(100) NOT NULL,
+            street VARCHAR(255) NOT NULL,
+            pincode VARCHAR(20),
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+        );`;
+        const result = yield client.query(query);
+        console.log(result);
+    }
+    catch (error) {
+        console.log(error);
+    }
+    finally {
+        yield client.end();
+    }
+});
+const insertInAddressTable = (user_id, city, country, street, pincode) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield client.connect();
+        const query = "INSERT INTO address (user_id, city, country, street, pincode) VALUES ($1, $2, $3, $4, $5);";
+        const result = yield client.query(query, [user_id, city, country, street, pincode]);
+        console.log(result);
+    }
+    catch (error) {
+        console.log(error);
+    }
+    finally {
+        yield client.end();
+    }
+});
+const getUserAndAddress = (userId) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield client.connect();
+        const query = `
+            SELECT u.id, u.username, u.email, a.city, a.country, a.street, a.pincode
+            FROM users u
+            JOIN address a ON u.id = a.user_id
+            WHERE u.id = $1;`;
+        const result = yield client.query(query, [userId]);
+        console.log(result.rows);
+    }
+    catch (error) {
+        console.log(error);
+    }
+    finally {
+        yield client.end();
+    }
+});
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,11 @@ import { Client } from "pg";
 import dotenv from 'dotenv'
 dotenv.config();
 
-
 const client = new Client({
     connectionString: process.env.POSTGRE
 })
 
-
-const createTable = async () => {    
+const createUsersTable = async () => {    
     try {
         await client.connect();
 
@@ -20,7 +18,7 @@ const createTable = async () => {
                 password VARCHAR(255) NOT NULL,
                 created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
             );    
-        `)
+        `);
         console.log(result);
 
     } catch(error) {
@@ -31,17 +29,12 @@ const createTable = async () => {
     }
 }
 
-createTable();
-
-
-const insertInTable = async (username: string, email: string, password: string) => {
+const insertInUserTable = async (username: string, email: string, password: string) => {
     try {
-        // await client.connect()
-        
-        const result = await client.query(`
-            INSERT INTO users (username, email, password) 
-            VALUES (hi, hi@gmail.com, 123);
-        `);
+        await client.connect();
+
+        const insertQuery = 'INSERT INTO users (username, email, password) VALUES ($1, $2, $3);'
+        const result = await client.query(insertQuery, [username, email, password]);
         console.log(result);
 
     } catch (error) {
@@ -52,4 +45,96 @@ const insertInTable = async (username: string, email: string, password: string) 
     }
 }
 
-insertInTable('aks', 'aks@gmail.com', '1_69420')
+const getUser = async (email: string) => {
+    try{
+        await client.connect();
+
+        const getUserQuery = 'SELECT * FROM users WHERE email = $1'
+        const result = await client.query(getUserQuery, [email]);
+
+        if(result.rows.length > 0){
+            console.log("User found:", result.rows[0]);
+            return;
+        }
+        else{
+            console.log(`No user found with the given email.`);
+            return null;
+        }
+    }
+    catch(error){
+        console.log(error);
+    }
+    finally{
+        await client.end();
+    }
+}
+
+const createAddressTable = async () => {
+    try {
+        await client.connect();
+
+        const query = `CREATE TABLE address (
+            id SERIAL PRIMARY KEY,
+            user_id INTEGER NOT NULL,
+            city VARCHAR(100) NOT NULL,
+            country VARCHAR(100) NOT NULL,
+            street VARCHAR(255) NOT NULL,
+            pincode VARCHAR(20),
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+        );`
+        const result = await client.query(query);
+        console.log(result);
+
+    } catch (error) {
+        console.log(error);
+    }
+    finally{
+        await client.end();
+    }
+}
+
+const insertInAddressTable = async (user_id: number, city: string, country: string, street: string, pincode: string) => {
+    try {
+        await client.connect();
+
+        const query = "INSERT INTO address (user_id, city, country, street, pincode) VALUES ($1, $2, $3, $4, $5);"
+        const result = await client.query(query, [user_id, city, country, street, pincode])
+        console.log(result);
+         
+    } catch (error) {
+        console.log(error);
+    }
+    finally{
+        await client.end();
+    }
+}
+
+const getUserAndAddress = async (userId: number) => {
+    try {
+        await client.connect();
+
+        const query = `
+            SELECT u.id, u.username, u.email, a.city, a.country, a.street, a.pincode
+            FROM users u
+            JOIN address a ON u.id = a.user_id
+            WHERE u.id = $1;`
+        const result = await client.query(query, [userId])
+        console.log(result.rows);
+        
+    }
+    catch (error) {
+        console.log(error);
+    }
+    finally{
+        await client.end();
+    }
+}
+
+// createUsersTable();
+// insertInUserTable('ep', 'ep@gmail.com', '3_69420');
+// getUser('hk@gmail.com');
+
+// createAddressTable();
+// insertInAddressTable(2, 'Delhi', 'India', 'GHPS', '94');
+// getUserAndAddress(2);
